666e7c70482f1895b6162629ca1060f1099e335c
YOON HYUNGSOO <yhyungs66@gmail.com>
2015-08-28 06:42:11 +0000

    devise setting2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+   registrations: 'users/registrations'
+   
+  }
+  
   get 'home/index'
-  root 'home#index'
   get 'home/tables_simple'
   get 'home/data'
   get 'home/dashboard'
+  
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
 end
